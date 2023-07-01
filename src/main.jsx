f4475b13b64327ad8d900ce76b18ddf3510f1330
bojanciko <122114035+bojanciko@users.noreply.github.com>
2023-07-01 14:20:38 +0200

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App.jsx'
-import { store } from './services/Store.js'
+import { store } from './services/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
